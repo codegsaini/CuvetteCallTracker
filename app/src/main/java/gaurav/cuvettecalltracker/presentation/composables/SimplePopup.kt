3c4c205402fa8481d1e2d2c59e556cd5d6265189
Gaurav Saini <codegsaini@gmail.com>
2024-12-14 17:55:10 +0530

    fix: Optimize permission flow

---
+++ app/src/main/java/gaurav/cuvettecalltracker/presentation/composables/SimplePopup.kt
@@ -0,0 +1,82 @@
+package gaurav.cuvettecalltracker.presentation.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+
+@Composable
+fun SimplePopup(
+    label: String = "Unlabeled",
+    message: String = "No Message",
+    onAction: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    Popup(
+        alignment = Alignment.Center,
+        onDismissRequest = onDismissRequest,
+        properties = PopupProperties(
+            dismissOnClickOutside = false,
+            dismissOnBackPress = false
+        )
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color(0x4B000000)),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                Modifier
+                    .widthIn(max = 300.dp)
+                    .fillMaxWidth()
+                    .shadow(3.dp, RoundedCornerShape(15.dp))
+                    .background(Color.White, RoundedCornerShape(15.dp))
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Text(
+                    text = label,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 0.sp
+                )
+                Text(
+                    text = message,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center
+                )
+                Button(
+                    onClick = onAction,
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.White,
+                        containerColor = Color.Black,
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text("Allow from Setting")
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/gaurav/cuvettecalltracker/presentation/home/HomeScreen.kt
+++ app/src/main/java/gaurav/cuvettecalltracker/presentation/home/HomeScreen.kt
@@ -1,28 +1,51 @@
 package gaurav.cuvettecalltracker.presentation.home
 
 import android.Manifest
+import android.app.Application
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,6 +54,7 @@ import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import gaurav.cuvettecalltracker.presentation.composables.CallLogCard
 import gaurav.cuvettecalltracker.presentation.composables.Label
+import gaurav.cuvettecalltracker.presentation.composables.SimplePopup
 import gaurav.cuvettecalltracker.presentation.home.composables.AnalyticsRow
 import gaurav.cuvettecalltracker.presentation.util.CallType
 import gaurav.cuvettecalltracker.presentation.util.findActivity
@@ -39,6 +63,14 @@ var permissionsGrantMap = mutableMapOf(
     Manifest.permission.READ_PHONE_STATE to false,
     Manifest.permission.READ_CALL_LOG to false
 )
+
+val simplePermissionTitle = mapOf(
+    Manifest.permission.READ_CALL_LOG to "READ CALL LOG",
+    Manifest.permission.READ_PHONE_STATE to "READ PHONE STATE"
+)
+
+fun getSimplePermissionTitle(list: List<String>) =
+    list.map { simplePermissionTitle[it] }.joinToString(", ")
 
 
 @Composable
@@ -49,12 +81,10 @@ fun HomeScreen(
 
     val context = LocalContext.current
     val activity = context.findActivity()
-    var permissionOfCurrentRationale by remember { mutableStateOf("") }
-    val singlePermissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission()
-    ) { granted ->
-        permissionsGrantMap[permissionOfCurrentRationale] = granted
-    }
+    val shouldShowPermissionRationalePermissions = remember { mutableStateListOf<String>() }
+    val permanentlyDeniedPermissions = remember { mutableStateListOf<String>() }
+    var showPermissionRationalePopup by remember { mutableStateOf(false) }
+    var showGoToSettingsPopup by remember { mutableStateOf(false) }
     val permissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
@@ -62,14 +92,19 @@ fun HomeScreen(
             permissionsGrantMap[permission] = granted
             if (!granted) {
                 activity?.let {
-                    if (shouldShowRequestPermissionRationale(it, permission))
-                        permissionOfCurrentRationale = permission
+                    if (shouldShowRequestPermissionRationale(it, permission)) {
+                        shouldShowPermissionRationalePermissions.add(permission)
+                        permanentlyDeniedPermissions.remove(permission)
+                    }
                     else {
-
+                        permanentlyDeniedPermissions.add(permission)
+                        shouldShowPermissionRationalePermissions.remove(permission)
                     }
                 }
             }
         }
+        showPermissionRationalePopup = shouldShowPermissionRationalePermissions.toList().isNotEmpty()
+        showGoToSettingsPopup = permanentlyDeniedPermissions.toList().isNotEmpty()
     }
 
     val recentLogs = viewModel.state.value.recentLogs
@@ -81,32 +116,34 @@ fun HomeScreen(
 
     val lifeCycleOwner = LocalLifecycleOwner.current
 
-    LaunchedEffect(Unit) {
+    fun checkPermissions() {
+        val updatedMap = mutableMapOf<String, Boolean>()
         permissionsGrantMap.forEach { (permission, _) ->
-            permissionsGrantMap[permission] = ContextCompat.checkSelfPermission(
+            val granted = ContextCompat.checkSelfPermission(
                 context,
                 permission
             ) == PackageManager.PERMISSION_GRANTED
+            updatedMap[permission] = granted
+            if (granted) {
+                shouldShowPermissionRationalePermissions.remove(permission)
+                permanentlyDeniedPermissions.remove(permission)
+            }
         }
+        showPermissionRationalePopup = shouldShowPermissionRationalePermissions.toList().isNotEmpty()
+        showGoToSettingsPopup = permanentlyDeniedPermissions.toList().isNotEmpty()
+        permissionsGrantMap = updatedMap
+    }
+
+    LaunchedEffect(Unit) {
+        checkPermissions()
     }
 
     DisposableEffect(lifeCycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
             if (event == Lifecycle.Event.ON_START) {
-                if (permissionsGrantMap.all { (_, granted) -> !granted })
+                if (permissionsGrantMap.any { (_, granted) -> !granted })
                     permissionLauncher.launch(permissionsGrantMap.keys.toTypedArray())
-                else
-                    permissionsGrantMap.forEach { (permission, granted) ->
-                        if (!granted)
-                            activity?.let {
-                                if (shouldShowRequestPermissionRationale(it, permission))
-                                    permissionOfCurrentRationale = permission
-                                else {
-
-                                }
-                            }
-                }
-            }
+            } else if (event == Lifecycle.Event.ON_RESUME) checkPermissions()
         }
         lifeCycleOwner.lifecycle.addObserver(observer)
         onDispose {
@@ -114,7 +151,35 @@ fun HomeScreen(
         }
     }
 
-    pe
+    fun openSettings() {
+        Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", context.applicationContext.packageName, null)
+        ).also {
+            context.startActivity(it)
+        }
+    }
+
+    if (showPermissionRationalePopup) {
+        SimplePopup(
+            label = "Message",
+            message = "The app need " +
+                    getSimplePermissionTitle(shouldShowPermissionRationalePermissions.toList()) +
+                    " permissions to function normally",
+            onAction = { permissionLauncher.launch(shouldShowPermissionRationalePermissions.toTypedArray()) },
+            onDismissRequest = { showGoToSettingsPopup = false }
+        )
+    }
+    if (showGoToSettingsPopup) {
+        SimplePopup(
+            label = "Message",
+            message = "You have denied " +
+                    getSimplePermissionTitle(permanentlyDeniedPermissions.toList()) +
+                    " permission. Please allow permissions for app to function normally",
+            onAction = { openSettings() },
+            onDismissRequest = { showGoToSettingsPopup = false }
+        )
+    }
 
     LazyColumn(
         Modifier
